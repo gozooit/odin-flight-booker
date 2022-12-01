e373dd87ac68d69d522aeb62847b9603572bb107
gozooit <gozoo.it@protonmail.com>
2022-12-01 10:13:37 +0100

    validation of :code

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,2 +1,6 @@
 class Airport < ApplicationRecord
+  validates :code,
+            format: { with: /\A[A-Z]{3}\z/, message: 'only allow 3 capital letters' },
+            uniqueness: true,
+            presence: true
 end
