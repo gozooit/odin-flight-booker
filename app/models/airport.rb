f526db88cb37f1ce738975a210101d3b998657ec
gozooit <gozoo.it@protonmail.com>
2022-12-01 11:04:43 +0100

    set up association between airports and flights

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,4 +1,7 @@
 class Airport < ApplicationRecord
+  has_many :departing_flights, class_name: 'Flight', foreign_key: :departure_airport_id, dependent: :destroy
+  has_many :arriving_flights, class_name: 'Flight', foreign_key: :arrival_airport, dependent: :destroy
+
   validates :code,
             format: { with: /\A[A-Z]{3}\z/, message: 'only allow 3 capital letters' },
             uniqueness: true,
