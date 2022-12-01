bdf976ba4b2348ffdf2df40f2fa318284785d1d3
gozooit <gozoo.it@protonmail.com>
2022-12-01 13:53:17 +0100

    build index method, to be fixed

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,3 +1,18 @@
 class FlightsController < ApplicationController
+  def index
+    # Currently there is no :flight params, only params
+    # Probably caused by form_with model: @flight but @flight does not exists
+    # @flight = params[:flight] ? Flight.new(flight_params) : Flight.new
+    if params[:flight]
+      # delete empty params
+      flight_params.delete_if { |k, v| v.empty? || k == 'nb_passenger' }
+      @flights = (flight_params.empty? ? Flight.all : Flight.where(flight_params)).includes(:departure_airport, :arrival_airport).order(:start).limit(100)
+    end
+  end
 
+  private
+
+  def flight_params
+    params.require(:flight).permit(:departure_airport_id, :arrival_airport_id, :start, :duration)
+  end
 end
