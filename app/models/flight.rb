dbe6defc8d6c71a26bf1bb5b498f9859bc4ec648
gozooit <gozoo.it@protonmail.com>
2022-12-01 12:26:55 +0100

    format duration from sec to human readable

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,8 @@
 class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: 'Airport'
   belongs_to :arrival_airport, class_name: 'Airport'
+
+  def formatted_duration
+    Time.at(duration).utc.strftime('%H:%M:%S')
+  end
 end
