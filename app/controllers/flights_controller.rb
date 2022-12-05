f82af0dce2cb27b9313c736a363aa109e1217682
gozooit <gozoo.it@protonmail.com>
2022-12-05 12:45:04 +0100

    now search form is working as intended

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,20 +1,37 @@
 class FlightsController < ApplicationController
+
+  # def index
+  #   @flight = Flight.new
+  #   if params[:flight].empty?
+  #     @flights = Flight.all
+  #   else
+  #     # delete empty params
+  #     params[:flight].delete_if { |k, v| v.empty? || k == :nb_passenger }
+  #     # params[:flight][:start] = params[:flight][:start].to_date.beginning_of_day..params[:flight][:start].to_date.end_of_day if params[:flight][:start]
+  #     flight_params[:start] = params[:flight][:start].to_date.all_day
+  #     @flights = Flight.where(departure_airport_id: flight_params[:departure_airport_id])
+  #                      .includes(:departure_airport, :arrival_airport)
+  #                      .order(:start)
+  #                      .limit(100)
+  #   end
+  # end
+
+  # rubocop:disable Metrics
   def index
     @flight = Flight.new
-    # Currently there is no :flight params, only params
-    # Probably caused by form_with model: @flight but @flight does not exists
-    # @flight = params[:flight] ? Flight.new(flight_params) : Flight.new
-    if params[:flight]
-      # delete empty params
-      params[:flight].delete_if { |k, v| v.empty? || k == :nb_passenger }
-      # params[:flight][:start] = params[:flight][:start].to_date.beginning_of_day..params[:flight][:start].to_date.end_of_day if params[:flight][:start]
-      params[:flight][:start] = params[:flight][:start].to_date
-      @flights = (flight_params.empty? ? Flight.all : Flight.where(flight_params))
-                 .includes(:departure_airport, :arrival_airport)
-                 .order(:start)
-                 .limit(100)
-    end
+    return unless params[:flight]
+
+    params[:flight].delete_if { |k, v| v.empty? || k == :nb_passenger }
+    query = flight_params
+    query[:start] = query[:start].to_date.all_day if query[:start]
+    return @flights = Flight.all if query.empty?
+
+    @flights = Flight.where(query)
+                     .includes(:departure_airport, :arrival_airport)
+                     .order(:start)
+                     .limit(100)
   end
+  # rubocop:enable Metrics
 
   private
 
