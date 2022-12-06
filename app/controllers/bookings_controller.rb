faa7981d7a91629ace619c164c35bf41b5a87b03
gozooit <gozoo.it@protonmail.com>
2022-12-06 12:17:14 +0100

    method to create and show

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,26 @@
 class BookingsController < ApplicationController
   def new
     @flight = Flight.find(params[:id])
+    @booking = Booking.new
     render new_booking_path if @flight
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to @booking, notice: 'Booking was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:flight_id, passengers_attributes: %i[name email])
   end
 end
