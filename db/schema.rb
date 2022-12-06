304309572270d517e328e93ee1840c33c5ce1d66
gozooit <gozoo.it@protonmail.com>
2022-12-06 09:55:14 +0100

    create and migrate Booking model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_01_093036) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_06_085429) do
   create_table "airports", force: :cascade do |t|
     t.string "code"
     t.string "name"
@@ -19,6 +19,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_01_093036) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["code"], name: "index_airports_on_code", unique: true
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.integer "flight_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["flight_id"], name: "index_bookings_on_flight_id"
   end
 
   create_table "flights", force: :cascade do |t|
@@ -32,6 +39,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_01_093036) do
     t.index ["departure_airport_id"], name: "index_flights_on_departure_airport_id"
   end
 
+  add_foreign_key "bookings", "flights"
   add_foreign_key "flights", "airports", column: "arrival_airport_id"
   add_foreign_key "flights", "airports", column: "departure_airport_id"
 end
