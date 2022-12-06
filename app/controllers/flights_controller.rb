61d6c978fb983625521a23a2f8d33f1859ff81b9
gozooit <gozoo.it@protonmail.com>
2022-12-06 10:04:34 +0100

    save nb_passenger in variable

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,26 +1,10 @@
 class FlightsController < ApplicationController
-
-  # def index
-  #   @flight = Flight.new
-  #   if params[:flight].empty?
-  #     @flights = Flight.all
-  #   else
-  #     # delete empty params
-  #     params[:flight].delete_if { |k, v| v.empty? || k == :nb_passenger }
-  #     # params[:flight][:start] = params[:flight][:start].to_date.beginning_of_day..params[:flight][:start].to_date.end_of_day if params[:flight][:start]
-  #     flight_params[:start] = params[:flight][:start].to_date.all_day
-  #     @flights = Flight.where(departure_airport_id: flight_params[:departure_airport_id])
-  #                      .includes(:departure_airport, :arrival_airport)
-  #                      .order(:start)
-  #                      .limit(100)
-  #   end
-  # end
-
   # rubocop:disable Metrics
   def index
     @flight = Flight.new
     return unless params[:flight]
 
+    @nb_passenger = params[:flight][:nb_passenger]
     params[:flight].delete_if { |k, v| v.empty? || k == :nb_passenger }
     query = flight_params
     query[:start] = query[:start].to_date.all_day if query[:start]
@@ -32,10 +16,6 @@ class FlightsController < ApplicationController
                      .limit(100)
   end
   # rubocop:enable Metrics
-
-  def new
-    @flight = Flight.new
-  end
 
   private
 
