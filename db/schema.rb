c7046ae991bbffa77baedf70538e5259a76535c6
gozooit <gozoo.it@protonmail.com>
2022-12-06 09:57:19 +0100

    create and migrate Passenger model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_06_085429) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_06_085637) do
   create_table "airports", force: :cascade do |t|
     t.string "code"
     t.string "name"
@@ -39,7 +39,17 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_06_085429) do
     t.index ["departure_airport_id"], name: "index_flights_on_departure_airport_id"
   end
 
+  create_table "passengers", force: :cascade do |t|
+    t.integer "booking_id", null: false
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["booking_id"], name: "index_passengers_on_booking_id"
+  end
+
   add_foreign_key "bookings", "flights"
   add_foreign_key "flights", "airports", column: "arrival_airport_id"
   add_foreign_key "flights", "airports", column: "departure_airport_id"
+  add_foreign_key "passengers", "bookings"
 end
